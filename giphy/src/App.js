26b8a335b139e4780d3809f8ec58132a27ff25af
DannieC97 <danielchabi97@gmail.com>
2022-01-13 17:41:37 -0500

    implemented search bar, and gifs, connected search bar to the API, closes #4,#5, and #6

--- giphy/src/App.js
+++ giphy/src/App.js
@@ -3,23 +3,44 @@ import './App.css';
 import SearchField from'./SearchField' 
 import GiftCard from './GIftCard';
 
+
 function App() {
+const [code,setCode] =useState("")
 const [data,setData] = useState([])
+let trending = false
+
+function filter(data){
+  data.map((item)=>{
+    return <img key={item.id}src={item.images.original.url} />
+ 
+})
+}
+
 useEffect(async ()=>{
   (async()=>{
-    const response = await fetch(`https://api.giphy.com/v1/gifs/search?q=ladder&limit=5&api_key=g5GU1WDPDqLyo6rsrWc3vJeQ3qypwP86`);
+    const response = await fetch(`https://api.giphy.com/v1/gifs/search?q=${code}&limit=20&api_key=g5GU1WDPDqLyo6rsrWc3vJeQ3qypwP86`);
     const data = await response.json();
-    console.log(data)
-    setData(data.data);
+    console.log(data);
+    let res = data.data
+    if(trending){
+      
+      let filteredData = Object.filter(res, x => x == "pg")
+      setData(filteredData);
+    } else {
+    setData(res);
+    }
   })();
-},[])
+},[code])
+console.log(code)
   return (
     <div className="App">
-     <p>test</p>
+     <SearchField 
+     setCode={setCode} />
      
      
      <GiftCard
-     data= {data} />
+     data= {data} 
+     />
      
     </div>
   );
