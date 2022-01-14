238f00a51cf240e584a2c4d1287f08cfdbc4de4a
DannieC97 <danielchabi97@gmail.com>
2022-01-14 15:56:49 -0500

    added a rating filter closes #8

--- giphy/src/SearchField.js
+++ giphy/src/SearchField.js
@@ -3,13 +3,42 @@ import GiftCard from './GIftCard'
 
 function SearchField(props){
     
-   const {setCode} = props
+   const setCode = props.setCode
+   const setRate = props.setRate
+   const setTrending = props.setTrending
     //make this asynch to be able to use await?
     function handleSearch(event){
         setCode(event.target.value)
         
         
     }
+    function changeRating(value) {
+    
+  
+        if (value === "none") {
+          setTrending(false)
+          console.log("rate")
+          setRate("")
+          
+        }
+        if (value === "g") {
+          
+          setRate(value)
+          setTrending(true)
+          console.log("rateg")
+        }
+        if (value === "pg") {
+          setRate(value)
+          setTrending(true)
+        }
+        if (value === "pg-13") {
+          setRate(value)
+          setTrending(true)
+          
+        }
+        
+      }
+    
     
     
     
@@ -17,7 +46,12 @@ function SearchField(props){
         <div id="searchDiv">
             
             <input type="search" className="bar" name="inpt" placeholder='Search'  onChange={handleSearch}></input>
-            
+            <select name="rating" id="rating"  onChange={(val) => changeRating(val.target.value)}>
+        <option value="none" >none</option>
+        <option value="g">g</option>
+        <option value="pg">pg</option>
+        <option value="pg-13">pg-13</option>
+      </select>
            
         </div>
     )
